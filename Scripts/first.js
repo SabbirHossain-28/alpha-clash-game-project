dd856128de96bd7ce87219b1d6343d3f30d7b93e
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-02-15 04:40:25 +0600

    create some keyboard and random letter functionalities

---
+++ Scripts/first.js
@@ -0,0 +1,31 @@
+function pageSwitcher(switchPageID1,switchPageID2){
+    const landingPage=document.getElementById(switchPageID1);
+    landingPage.classList.add('hidden');
+    const playGround=document.getElementById(switchPageID2);
+    playGround.classList.remove('hidden');
+}
+function getRandomLetter(){
+    const alphabetString='abcdefghijklmnopqrstuvwxyz';
+    const alphabets=alphabetString.split('');
+    
+    const randomNumber=Math.random()*25;
+    const indexNumber=Math.round(randomNumber);
+
+    const alphabet=alphabets[indexNumber];
+    return(alphabet);
+}
+function setBackgroundColorById(letterid){
+    const x=letterid.innerText.toLowerCase();
+    const displayletter=document.getElementById(x);
+    displayletter.classList.add('bg-orange-500');
+}
+function continueGame(){
+    const letter =getRandomLetter();
+    const changeLetter=document.getElementById('letterId');
+    changeLetter.innerText=letter;
+    setBackgroundColorById(changeLetter);
+}
+function clickToSwitch(){
+    pageSwitcher('landingPage','playground');
+    continueGame();
+}
